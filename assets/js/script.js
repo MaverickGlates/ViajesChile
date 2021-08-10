442223cf0fe61a600fb5b2303c0a5461dd7e1f92
Alexander <alexander.maverick1996@gmail.com>
2021-08-10 00:31:26 -0400

    last_version_10082021

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,15 @@ $(function () {
     });
 //---------------------------------------------------- OCULTAR ICONOS ----------------------------------------------------     
     $(".ocultar-mostrar").click(function(){
-        $(".ocultar-mostrar").slideToggle(1500); 
+        $(".ocultar-mostrar").slideToggle(600); 
         $(".ocultar-mostrar").toggle('show');
     });
     $(".ocultar-mostrar2").click(function(){
-        $(".ocultar-mostrar2").slideToggle(1500); 
+        $(".ocultar-mostrar2").slideToggle(600); 
         $(".ocultar-mostrar2").toggle('show');
     });
     $(".ocultar-mostrar3").click(function(){
-        $(".ocultar-mostrar3").slideToggle(1500); 
+        $(".ocultar-mostrar3").slideToggle(600); 
         $(".ocultar-mostrar3").toggle('show');
     });
 //---------------------------------------------------- DELAY CAROUSEL ----------------------------------------------------  
